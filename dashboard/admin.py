9b99988f04496fbadb3804917b95950c314f3251
Aniol Marti <aniol@aniolmarti.cat>
2020-02-28 23:21:02 +0100

    Display Video in admin dashboard

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from dashboard.models import Video
+
 
 # Register your models here.
+class VideoAdmin(admin.ModelAdmin):
+    list_display = ('nom', 'url', 'thumbnail')
+    exclude = ()
+
+
+admin.site.register(Video, VideoAdmin)
--- dashboard/models.py
+++ dashboard/models.py
@@ -6,5 +6,5 @@ class Video(models.Model):
     MAX_LENGTH = 200
     id = models.AutoField(primary_key=True)
     nom = models.CharField(max_length=MAX_LENGTH)
-    url = models.URLField(max_length=MAX_LENGTH)
+    url = models.URLField(max_length=MAX_LENGTH, unique=True)
     thumbnail = models.ImageField(max_length=MAX_LENGTH, blank=True)
