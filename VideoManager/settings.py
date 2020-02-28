d55f6ced0e776b1a5f4b4c73c23eaf98e45f1b8f
Aniol Marti <aniol@aniolmarti.cat>
2020-02-28 22:47:07 +0100

    Add secret settings

--- VideoManager/settings.py
+++ VideoManager/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+try:
+    from .settings_secret import *
+except:
+    print ("Missing settings_secret.py file.")
+    quit()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
