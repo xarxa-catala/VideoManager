da4c1a6676a502cd2d6a1ede41e10c85812bcacf
Aniol Marti <aniol@aniolmarti.cat>
2020-02-28 22:57:53 +0100

    Add video model

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
+class Video(models.Model):
+    MAX_LENGTH = 200
+    id = models.AutoField(primary_key=True)
+    nom = models.CharField(max_length=MAX_LENGTH)
+    url = models.URLField(max_length=MAX_LENGTH)
+    thumbnail = models.ImageField(max_length=MAX_LENGTH, blank=True)
