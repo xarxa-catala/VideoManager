bef57a4e5abfef91a852c6d4d3eadbfb3af68e56
Aniol Marti <aniol@aniolmarti.cat>
2020-02-29 12:47:06 +0100

    End models definition

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,11 +1,30 @@
 from django.contrib import admin
-from dashboard.models import Video
+from dashboard.models import Video, Show, Season, VideoType
 
 
 # Register your models here.
-class VideoAdmin(admin.ModelAdmin):
-    list_display = ('nom', 'url', 'thumbnail')
+class ShowAdmin(admin.ModelAdmin):
+    list_display = ('nom_curt', 'nom')
     exclude = ()
 
 
+class SeasonAdmin(admin.ModelAdmin):
+    list_display = ('nom', 'show')
+    exclude = ()
+
+
+class VideoTypeAdmin(admin.ModelAdmin):
+    list_display = ('nom',)
+    exclude = ()
+
+
+class VideoAdmin(admin.ModelAdmin):
+    list_display = ('nom', 'url', 'thumbnail')
+    readonly_fields = ('url',)
+    exclude = ()
+
+
+admin.site.register(Show, ShowAdmin)
+admin.site.register(Season, SeasonAdmin)
+admin.site.register(VideoType, VideoTypeAdmin)
 admin.site.register(Video, VideoAdmin)
--- dashboard/models.py
+++ dashboard/models.py
@@ -2,9 +2,44 @@ from django.db import models
 
 
 # Create your models here.
+class Show(models.Model):
+    MAX_LENGTH = 200
+    id = models.AutoField(primary_key=True)
+    nom = models.CharField(max_length=MAX_LENGTH)
+    nom_curt = models.CharField(max_length=MAX_LENGTH, unique=True, null=True)
+
+    def __str__(self):
+        return self.nom
+
+
+class Season(models.Model):
+    MAX_LENGTH = 200
+    id = models.AutoField(primary_key=True)
+    nom = models.CharField(max_length=MAX_LENGTH)
+    show = models.ForeignKey(Show, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.show.nom + " - " + self.nom
+
+
+class VideoType(models.Model):
+    MAX_LENGTH = 200
+    id = models.AutoField(primary_key=True)
+    nom = models.CharField(max_length=MAX_LENGTH)
+
+    def __str__(self):
+        return self.nom
+
+
 class Video(models.Model):
     MAX_LENGTH = 200
     id = models.AutoField(primary_key=True)
     nom = models.CharField(max_length=MAX_LENGTH)
+    show = models.ForeignKey(Show, on_delete=models.CASCADE, null=True)
+    season = models.ForeignKey(Season, on_delete=models.CASCADE, null=True, blank=True)
+    episodi = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True,
+                                help_text="Episodi associat a la preqüela o seqüela.")
+    tipus = models.ForeignKey(VideoType, on_delete=models.CASCADE, null=True)
     url = models.URLField(max_length=MAX_LENGTH, unique=True)
+    fitxer = models.FileField(max_length=MAX_LENGTH, null=True)
     thumbnail = models.ImageField(max_length=MAX_LENGTH, blank=True)
