467c118fd26a42ddf5c6d833ae5e78ae683e8a1c
Aniol Marti <aniol@aniolmarti.cat>
2020-03-07 13:06:29 +0100

    Add a signal to generate the video URL

--- dashboard/utils/signals.py
+++ dashboard/utils/signals.py
@@ -1,3 +1,12 @@
 from django.db.models.signals import pre_save
 from django.dispatch.dispatcher import receiver
 from dashboard.models import Video
+from .move_file import move_file
+from VideoManager.constants import *
+import os
+
+
+@receiver(pre_save, sender=Video)
+def generate_url(sender, instance, **kwargs):
+    filename = os.path.basename(instance.fitxer.name)
+    instance.video_url = os.path.join(URL, move_file(instance, filename))
