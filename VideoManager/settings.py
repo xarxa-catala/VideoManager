f786f3bef1c1f641d48c686a2e18f54eb56fd2b8
Aniol Marti <aniol@aniolmarti.cat>
2020-04-13 13:27:02 +0200

    Use MySQL instead of sqlite

--- VideoManager/settings.py
+++ VideoManager/settings.py
@@ -85,8 +85,11 @@ WSGI_APPLICATION = 'VideoManager.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': DATABASE_MYSQL,
+        'USER': USER_NAME,
+        'PASSWORD': PASSWORD,
+        'HOST': HOST,
     }
 }
 
