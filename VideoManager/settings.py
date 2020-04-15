03d6a3b6f3c701aa1f40f098e165f09760cbb029
Aniol Marti <aniol@aniolmarti.cat>
2020-04-15 20:29:09 +0200

    Disable debugging

--- VideoManager/settings.py
+++ VideoManager/settings.py
@@ -31,7 +31,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = SECRET_KEY_SAVED
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['gestio.multimedia.xarxacatala.cat', '127.0.0.1']
 
