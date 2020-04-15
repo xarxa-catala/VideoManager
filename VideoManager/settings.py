64c86d206e27fdffe479e0e89f7078645c06c1fa
Aniol Marti <aniol@aniolmarti.cat>
2020-04-15 20:09:54 +0200

    Also allow localhost

--- VideoManager/settings.py
+++ VideoManager/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = SECRET_KEY_SAVED
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['gestio.multimedia.xarxacatala.cat']
+ALLOWED_HOSTS = ['gestio.multimedia.xarxacatala.cat', '127.0.0.1']
 
 
 # Application definition
