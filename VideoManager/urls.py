2adde9334d7f20f9e30d3ba3773d26b6e1dad803
Aniol Marti <aniol@aniolmarti.cat>
2020-06-28 17:43:57 +0200

    Delete d/urls and d/views

--- VideoManager/urls.py
+++ VideoManager/urls.py
@@ -20,7 +20,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', admin.site.urls),
-    path('', include('dashboard.urls')),
     path('api/', include('api.urls')),
 ]
 
--- dashboard/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('cua/', views.queue_view),
-]
