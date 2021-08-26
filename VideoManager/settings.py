519c3fd949098bb77351c9656a5e4f1011490c67
Aniol Martí <aniol@aniolmarti.cat>
2021-08-27 00:16:54 +0200

    Enable CORS for all domains

--- VideoManager/settings.py
+++ VideoManager/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -62,6 +63,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'VideoManager.urls'
 
